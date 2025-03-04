1f1e52448412f090d776744a50d939398aaabcbd
Михаил Владимиров <mikhail.vladimirov@urfu.me>
2025-03-04 16:07:15 +0500

    1

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
-const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
-const {readLine} = require('./console');
-
-const files = getFiles();
+const { getAllFilePathsWithExtension, readFile } = require('./fileSystem');
+const { readLine } = require('./console');
 
 console.log('Please, write your command!');
 readLine(processCommand);
 
 function getFiles() {
     const filePaths = getAllFilePathsWithExtension(process.cwd(), 'js');
-    return filePaths.map(path => readFile(path));
+    return filePaths.map(filePath => ({
+        filePath,
+        content: readFile(filePath),
+    }));
+}
+
+function extractTodosFromFiles() {
+    const files = getFiles();
+    const todoRegex = /\/\/\s*TODO\s*(.*)/i;
+
+    let todos = [];
+    for (const { filePath, content } of files) {
+        const lines = content.split('\n');
+        for (const line of lines) {
+            const match = line.match(todoRegex);
+            if (match) {
+                todos.push(match[1].trim());
+            }
+        }
+    }
+    return todos;
 }
 
 function processCommand(command) {
@@ -16,10 +34,14 @@ function processCommand(command) {
         case 'exit':
             process.exit(0);
             break;
+
+        case 'show':
+            const todos = extractTodosFromFiles();
+            todos.forEach(todo => console.log(todo));
+            break;
+
         default:
             console.log('wrong command');
             break;
     }
 }
-
-// TODO you can do it!
